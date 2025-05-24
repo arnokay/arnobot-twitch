08f439aa41fe1aa46655ce7a26baf204e49a6cbf
arnokay <arnokay.work@gmail.com>
2025-05-25 00:15:41 +0300

    feat: add app.Start and required functions to load echo\message broker\etc.

---
+++ cmd/server.go
@@ -0,0 +1,153 @@
+package main
+
+import (
+	"arnobot-shared/middlewares"
+	"arnobot-twitch/internal/config"
+	"context"
+	"errors"
+	"fmt"
+	"net/http"
+	"os"
+	"os/signal"
+	"sync"
+	"syscall"
+	"time"
+
+	"github.com/labstack/echo/v4"
+)
+
+func (app *application) Start() {
+	startError := make(chan error)
+	shutdownError := make(chan error)
+
+	go func() {
+		quit := make(chan os.Signal, 1)
+
+		signal.Notify(quit, syscall.SIGINT, syscall.SIGTERM)
+
+		s := <-quit
+
+		app.logger.Info("shutting down server", "signal", s.String())
+
+		ctx, cancel := context.WithTimeout(context.Background(), 20*time.Second)
+		defer cancel()
+
+		shutdownError <- app.Shutdown(ctx)
+	}()
+
+	go func() {
+		err := startAPIServer(app)
+		if err != nil {
+			startError <- err
+		}
+	}()
+
+	// go func() {
+	// 	err := startMBServer(app)
+	// 	if err != nil {
+	// 		startError <- err
+	// 	}
+	// }()
+	select {
+	case err := <-startError:
+		app.logger.Error("application start error", "err", err)
+	case err := <-shutdownError:
+		if err != nil {
+			app.logger.Error("application shutdown error", "err", err)
+		}
+	}
+	close(startError)
+	close(shutdownError)
+}
+
+func (app *application) Shutdown(ctx context.Context) error {
+	var wg sync.WaitGroup
+	errCh := make(chan error, 2)
+
+	wg.Add(1)
+	go func() {
+		defer wg.Done()
+		ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+		defer cancel()
+
+		app.logger.Debug("#shutdown.mb: gracefully closing mb connection")
+
+		done := make(chan struct{})
+		go func() {
+			err := app.msgBroker.Drain()
+			if err != nil {
+				errCh <- err
+			}
+			close(done)
+		}()
+
+		select {
+		case <-done:
+			app.logger.Debug("#shutdown.mb: gracefully closed mb connection")
+		case <-ctx.Done():
+			app.logger.Debug("#shutdown.mb: context timeout, force closing message broker connection")
+			app.msgBroker.Close()
+			app.logger.Debug("#shutdown:mb: force closed mb connection")
+		}
+	}()
+
+	wg.Add(1)
+	go func() {
+		defer wg.Done()
+		app.logger.Debug("#shutdown.api: gracefully closing api")
+		err := app.api.Shutdown(ctx)
+		if err != nil {
+			errCh <- err
+			return
+		}
+		app.logger.Debug("#shutdown.api: gracefully closed api")
+	}()
+
+	wg.Wait()
+	close(errCh)
+
+	app.logger.Debug("#shutdown.db: gracefully closing db")
+	app.db.Close()
+	app.logger.Debug("#shutdown.db: gracefully closed db")
+
+	for err := range errCh {
+		return err
+	}
+
+	return nil
+}
+
+// func startMBServer(a *application) error {
+// 	if a.msgBroker == nil {
+// 		return errors.New("startMBServer: msgBroker is nil")
+// 	}
+//
+// 	if a.msgBroker.IsClosed() {
+// 		return errors.New("startMBServer: msgBroker is closed")
+// 	}
+//
+// 	a.mbControllers.Connect(a.msgBroker)
+//
+// 	return nil
+// }
+
+func startAPIServer(a *application) error {
+	e := echo.New()
+
+	e.HideBanner = true
+	a.api = e
+
+	e.Use(middlewares.AttachTraceID)
+
+	mainGroup := e.Group("/v1")
+	a.apiControllers.Routes(mainGroup)
+
+	e.HTTPErrorHandler = middlewares.ErrHandler
+
+	err := e.Start(fmt.Sprintf(":%v", config.Config.Global.Port))
+	if err != nil && !errors.Is(err, http.ErrServerClosed) {
+		return err
+	}
+
+	return nil
+}
--- internal/config/config.go
+++ internal/config/config.go
@@ -2,13 +2,18 @@ package config
 
 import (
 	"flag"
+	"fmt"
 	"os"
+	"strconv"
+
+	"arnobot-shared/pkg/assert"
 )
 
 const (
 	ENV_DB_DSN    = "MB_URL"
 	ENV_WH_SECRET = "WH_SECRET"
 	ENV_BASE_URL  = "BASE_URL"
+	ENV_PORT      = "PORT"
 )
 
 type config struct {
@@ -18,8 +23,9 @@ type config struct {
 }
 
 type GlobalConfig struct {
-	LogLevel  int
-	BaseURL   string
+	LogLevel int
+	BaseURL  string
+	Port     int
 }
 
 type MBConfig struct {
@@ -35,10 +41,17 @@ var Config *config
 func Load() *config {
 	Config = &config{}
 
+	if os.Getenv(ENV_PORT) != "" {
+		port, err := strconv.Atoi(os.Getenv(ENV_PORT))
+		assert.NoError(err, fmt.Sprintf("%v: not a number", ENV_PORT))
+		Config.Global.Port = port
+	}
+
 	flag.StringVar(&Config.MB.URL, "mb-url", os.Getenv(ENV_DB_DSN), "Message Broker URL")
 	flag.IntVar(&Config.Global.LogLevel, "log-level", Config.Global.LogLevel, "Minimal Log Level (default: -4)")
 	flag.StringVar(&Config.Webhooks.Secret, "wh-secret", os.Getenv(ENV_WH_SECRET), "secrett for subscribing to webhooks")
 	flag.StringVar(&Config.Global.BaseURL, "base-url", os.Getenv(ENV_BASE_URL), "public url")
+	flag.IntVar(&Config.Global.Port, "port", Config.Global.Port, "http port")
 
 	flag.Parse()
 
