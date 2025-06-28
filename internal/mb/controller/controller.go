31b8ad9a8684b83f64bf745c49b00a40f72d0f1c
arnokay <arnokay.work@gmail.com>
2025-06-29 01:43:45 +0300

    feat: increase timeout on mb controllers from 5 to 60 seconds

--- internal/mb/controller/controller.go
+++ internal/mb/controller/controller.go
@@ -20,7 +20,7 @@ func (c *Controllers) Connect(conn *nats.Conn) {
 }
 
 func newControllerContext(traceID string) (context.Context, context.CancelFunc) {
-	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+	ctx, cancel := context.WithTimeout(context.Background(), 60*time.Second)
 	ctx = trace.Context(ctx, traceID)
 
 	return ctx, cancel
